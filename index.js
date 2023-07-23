34cd6a81a7f0b1a0df757b63a1bafc2362f4cbea
Adam Merjil <AMerjil@hotmail.com>
2023-07-22 20:43:30 -0700

    update 7

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const userDoc = await UserModel.findOne({ username });
+
+    if (!userDoc) {
+        return res.status(400).json("User not found");
+    }
+
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
         //logged in
